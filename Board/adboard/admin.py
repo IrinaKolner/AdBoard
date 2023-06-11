c0d42e445af1e666d62274a27864135b712e23b8
IrinaKolner <115484055+IrinaKolner@users.noreply.github.com>
2023-06-11 18:13:50 +0300

    initial commit

---
+++ Board/adboard/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Categories, Post, Author, Reply
+
+admin.site.register(Categories)
+admin.site.register(Post)
+admin.site.register(Author)
+admin.site.register(Reply)
+
+# admin.site.register(News)
+
+
